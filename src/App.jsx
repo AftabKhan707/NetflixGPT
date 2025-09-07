1b10709e374412de166b79e5d7b4745af79602e9
Aftab Khan <aftabsaboorkhan@gmail.com>
2025-09-06 23:57:18 -0400

    SignIn Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl font-bold">hello</div>;
+  return <Body />;
 }
 
 export default App;
