a9157d864712e743ac7b924276e70ceb2117f3f7
Aftab Khan <aftabsaboorkhan@gmail.com>
2025-09-08 04:05:09 -0400

    GPT Suggestions added

---
+++ src/components/GptSearchBar.jsx
@@ -0,0 +1,91 @@
+import { useDispatch, useSelector } from "react-redux";
+import lang from "../utils/languageConstants";
+import { useRef } from "react";
+import OpenAI from "openai";
+import { addGptMovieResult } from "../utils/gptSlice";
+
+const GptSearchBar = () => {
+  const langKey = useSelector((store) => store.config.lang);
+  const dispatch = useDispatch();
+  const searchText = useRef(null);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjRmYTNmYTEwYTU4ZWQxMzFmNmE0NjM1NTk2MGQyZiIsIm5iZiI6MTc1NzI3NTcxMS4xNjUsInN1YiI6IjY4YmRlNjNmM2MyYjE2MmJhMjFmNTJiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jvQYn7mDZZ78i7TVjG7Z2JcYPzlBNHbwnLQPHxRdV3A",
+    },
+  };
+
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      options
+    );
+    const json = await data.json();
+
+    return json.results;
+  };
+
+  const handleGptSearchClick = async () => {
+    // Make an API call to GPT API and get Movie Results
+    const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+    const client = new OpenAI({
+      apiKey: apiKey, // This is the default and can be omitted
+      dangerouslyAllowBrowser: true,
+    });
+
+    console.log("client", client);
+
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+
+    const gptResults = await client.chat.completions.create({
+      model: "gpt-4o",
+      messages: [{ role: "user", content: gptQuery }],
+    });
+
+    console.log(gptResults.choices?.[0]?.message?.content);
+
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    // [Promise, Promise, Promise, Promise, Promise]
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    // console.log(tmdbResults);
+
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
+  };
+
+  return (
+    <div className="pt-[10%] flex justify-center">
+      <form
+        className=" w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          type="text"
+          className=" p-4 m-4 col-span-9 bg-black text-white"
+          placeholder={lang[langKey].gptSearchPlaceholder}
+          ref={searchText}
+        />
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
+          {lang[langKey].search}
+        </button>
+      </form>
+    </div>
+  );
+};
+export default GptSearchBar;
